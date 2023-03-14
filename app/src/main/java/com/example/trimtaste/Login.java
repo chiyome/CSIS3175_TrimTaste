13a6538b4a80bd91dccc678cafcc9838225aa627
mayumi <yamayun0x0@gmail.com>
2023-03-14 15:26:17 -0700

    Second commit

---
+++ app/src/main/java/com/example/trimtaste/Login.java
@@ -0,0 +1,30 @@
+package com.example.trimtaste;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        EditText username = findViewById(R.id.edTxtUsername);
+        EditText password = findViewById(R.id.edTxtPassword);
+        Button btnLogin = findViewById(R.id.btnLogin);
+        Button btnToSignup = findViewById(R.id.btnToSignup);
+
+        btnToSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Login.this,Signup.class));
+            }
+        });
+    }
+}
